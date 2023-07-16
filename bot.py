664263c3429d46db139f061df87a64b752ca8e66
Charlies-Kvit <theivangao@gmail.com>
2023-07-16 21:05:40 +0300

    пофиксил баги, есть немного нового, но не особо

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import requests
+import random
 from other_functions import change_loyalty, check_time, check_warning_time
 from data import db_session
 from data.user import User
@@ -39,7 +41,43 @@ async def delete_user(message: Message):
     await message.answer(f"Пользователь {users_info[0].first_name} удален из базы данных")
 
 
-@bot.on.chat_message(text=['.отпуск <user> <days>', '.отпуск'])
+@bot.on.chat_message(OnlyAdmins(), text=['.удалить <user>', '.удалить'])
+async def delete_user_bd(message: Message, user=None):
+    if user is None:
+        await message.answer(
+            "Используйте следующий синтаксис: .удалить @user, то есть данная команда удалит из бд юзера")
+        return
+    user_id = int(user[3:user.find("|")])
+    users_info = await bot.api.users.get(user_id)
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).filter(User.login == users_info[0].id).first()
+    if not user:
+        db_sess.close()
+        await message.answer('Пользователь в бд не найден')
+        return
+    db_sess.delete(user)
+    db_sess.commit()
+    db_sess.close()
+    await message.answer(f"Пользователь {users_info[0].first_name} удален из базы данных")
+
+
+@bot.on.chat_message(OnlyAdmins(), text=['.проверить бд'])
+async def check_database(message: Message):
+    users_in_chat = await bot.api.messages.get_conversation_members(peer_id=message.peer_id)
+    users_id = [int(user.id) for user in users_in_chat.profiles]
+    db_sess = db_session.create_session()
+    users_from_bd = db_sess.query(User).all()
+    for user in users_from_bd:
+        if int(user.login) in users_id:
+            continue
+        db_sess.delete(user)
+        user_info = await bot.api.users.get(user.login)
+        await message.answer(f'Пользователь {user_info[0].first_name} {user_info[0].last_name} удален из бд')
+    db_sess.commit()
+    await message.answer("Из бд удалены все юзеры, не найденные в чате")
+
+
+@bot.on.chat_message(OnlyAdmins(), text=['.отпуск <user> <days>', '.отпуск'])
 async def take_vacation(message: Message, user=None, days=None):
     if user is None or days is None:
         await message.answer("Используйте следующий синтаксис: .отпуск <user> <days>\n"
@@ -52,7 +90,14 @@ async def take_vacation(message: Message, user=None, days=None):
     user.vacation = int(days)
     db_sess.commit()
     db_sess.close()
-    await message.answer(f"Пользователь {user_info[0].first_name} получил отпуск: {days}(вывод будет норм не в бете или в обнове)")
+    if days[-1] == '1' and int(days) != 11:
+        answer = 'день'
+    elif days[-1] in ['2', '3', '4']:
+        answer = 'дня'
+    else:
+        answer = "дней"
+    await message.answer(
+        f"Пользователь {user_info[0].first_name} получил отпуск на {days} {answer}")
 
 
 @bot.on.chat_message(OnlyAdmins(), text=['.отнять отпуск <user>', '.отнять отпуск'])
@@ -76,10 +121,16 @@ async def pick_up_vacation(message: Message, user=None):
     user.vacation = 0
     db_sess.commit()
     db_sess.close()
-    await message.answer(f"Отпуск у {user_info[0].first_name} успешно отнят")
+    await message.answer(f"Отпуск у {user_info[0].first_name} успешно аннулирован")
 
 
-@bot.loop_wrapper.interval(minutes=5)
+@bot.on.chat_message(OnlySuperAdmins(), text=['/send_message'])
+async def test_func(message: Message):
+    user_info = await bot.api.users.get(message.from_id)
+    await message.answer(f'@|{user_info[0].first_name}{user_info[0].last_name}')
+
+
+@bot.loop_wrapper.interval(minutes=4)
 async def auto_minus_loyalty():
     if check_time():
         db_sess = db_session.create_session()
@@ -96,6 +147,7 @@ async def auto_minus_loyalty():
             else:
                 user.reports_count = 0
                 user.unemployed_days = 0
+        await bot.api.messages.send(chat_id=0, random_id=0, message="Авто минус очков лояльности прошел успешно")
         db_sess.commit()
         db_sess.close()
 
@@ -106,11 +158,10 @@ async def user_warning():
         db_sess = db_session.create_session()
         warning_users = db_sess.query(User).filter(User.warning_user is True)
         for user in warning_users:
-            await bot.api.messages.send(user_id=user.login, random_id=0, message="Внимание! Уважаемый пользователь, вам"
-                                                                                 "нужно приступить к работе, иначе "
-                                                                                 "через"
-                                                                                 " два дня вы будете изгнаны автоматом,"
-                                                                                 " с уважением,\n legion_as(Легион)")
+            await bot.api.messages.send(chat_id=4, random_id=0, message=f"Внимание! Уважаемый пользователь, вам"
+                                                                        "нужно приступить к работе, иначе "
+                                                                        "через два дня вы будете изгнаны автоматом,"
+                                                                        " с уважением,\n legion_as(Легион)")
             user.warning_user = False
         db_sess.commit()
         db_sess.close()
@@ -189,7 +240,10 @@ async def get_help(message: Message):
     .удалить админа - удаляет админа из бд(только для админов с правами супер)\n
     .админы список - выдает список админов\n
     .изменить лояльность, .изм, .лоял, .лояльность - позваляет менять лояльность юзеру\n
-    .топ - выводит топ юзеров по лояльности\n
+    .рейтинг - выводит топ юзеров по лояльности\n
+    .удалить - удаляет юзера из базы данных\n
+    .проверить бд - удаляет всех из бд, кого нет в чате\n
+    .регистрация - добавляет в бд всех, кого нет в бд, но есть в чате\n
     .юзер - выводит инфу о конкретном юзере(Если хотите узнать о конкретном пользователе, то используйте следующий
     синтаксис: .юзер <user>, иначе вы получите инфу о себе)\n
     В остальных же случаях вы можете подробнее узнать о команде введя ее без параметров)""")
@@ -221,7 +275,7 @@ async def change_loyalty_user(message: Message, user=None, loyalty=None):
     db_sess.close()
 
 
-@bot.on.chat_message(OnlyAdmins(), text=['/топ', '.топ'])
+@bot.on.chat_message(OnlyAdmins(), text=['/топ', '.рейтинг'])
 async def get_top_users(message: Message):
     db_sess = db_session.create_session()
     users_info = db_sess.query(User).all()
@@ -232,7 +286,7 @@ async def get_top_users(message: Message):
     answer = ['Топ по очкам лояльности:']
     for num, key in enumerate(users_id):
         user_name = await bot.api.users.get(key)
-        answer.append(f"{num + 1}. {user_name[0].first_name}: {sorted_users[key]}")
+        answer.append(f"{num + 1}. {user_name[0].first_name} {user_name[0].last_name}: {sorted_users[key]}")
     await message.answer('\n'.join(answer))
 
 
@@ -266,12 +320,17 @@ async def get_user_info(message: Message, user=None):
     await message.answer(answer)
 
 
-@bot.on.chat_message(text=['чурбан', 'Чурбан'])
+@bot.on.chat_message(OnlyAdmins(), text=['чурбан', 'Чурбан'])
 async def meme(message: Message):
-    photo = await photo_uploader.upload(
-        file_source="churman.jpg"
-    )
-    await message.answer("✅ На месте", attachment=photo)
+    vars = [0, 1]
+    var = random.choice(vars)
+    if var == 0:
+        photo = await photo_uploader.upload(
+            file_source="churman.jpg"
+        )
+        await message.answer("✅ На месте", attachment=photo)
+    else:
+        await message.answer("САМ ЧУРБАН")
 
 
 @bot.on.chat_message(text=['.регистрация'])
@@ -291,10 +350,10 @@ async def registration(message: Message):
 
 @bot.on.chat_message()
 async def leave(message: Message):
-    # await message.answer(message.chat_id)
     session = db_session.create_session()
     session.close_all()
     print("all session close")
 
 
-bot.run_forever()
+if __name__ == '__main__':
+    bot.run_forever()
