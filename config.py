2dfbe6206e26a58af88e10bea5a49f54f22871b3
Charlies-Kvit <theivangao@gmail.com>
2023-07-31 15:16:01 +0300

    переписал бота на vkreal

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from api_key import token
 
 token = token
 db_session.global_init('db/db.sqlite')
-REPORTS_CHAT_ID = 2000000001  # 6 перед выкатом
+REPORTS_CHAT_ID = 2000000006  # 6 перед выкатом
 HOUR = 23
 MINUTES = 50
 WARNING_HOUR = 15
